d561c60b44ab8b2531efe4f92a7776fbd240874b
LannyWang009 <09legend.wang@gmail.com>
2018-10-02 01:29:43 -0500

    hwk 1 and hwk 2

--- exercises/01-predicate-functions.js
+++ exercises/01-predicate-functions.js
@@ -22,6 +22,27 @@
 // isVowel({e: 'Elephant'}) --> false
 
 
+//a function to check if an element is in an array
+function inArray (needle, haystack) {
+    for (var i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle){return true;}
+    }
+    return false;
+}  
+
+function isVowel (needle){
+    var haystack = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    //if(inArray(needle, haystack)) {return true} else {return false}; 35 and 36 are the same thing
+    return inArray(needle, haystack);
+}
+
+/*
+console.log( isVowel('c'));
+console.log( isVowel('e'));
+console.log( isVowel('A'));
+console.log( isVowel(99));
+console.log( isVowel('elephant'));
+*/
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write two functions: "isEven" and "isOdd" that take a number argument and
@@ -37,7 +58,26 @@
 // isOdd('7') --> false
 // isOdd(3.14) --> false
 
+function isEven (theNumber){
+    if (theNumber % 2 === 0) {
+        return true;
+    } else {return false};
+}
 
+function isOdd (theNumber) {
+    if ((theNumber % 2 === 1) || (theNumber % 2 === -1)) {
+        return true;
+    } else {return false};
+}
+/*
+console.log(isEven(100));
+console.log(isEven(1));
+console.log(isEven(-2));
+console.log(isEven('banana'));
+console.log(isOdd(5));
+console.log(isOdd(7));
+console.log(isOdd(3.14));
+*/
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "isCapitalCity" that takes two arguments: a US state and a city name
@@ -52,3 +92,21 @@
 // isCapitalCity('Texas', 'Houston') --> false
 // isCapitalCity('Alaska', 'Juneau') --> true
 // isCapitalCity('Strawberry', 'Mango') --> false
+
+const map = {"Alabama":"Montgomery","Alaska":"Juneau","Arizona":"Phoenix","Arkansas":"Little Rock","California":"Sacramento","Colorado":"Denver","Connecticut":"Hartford","Delaware":"Dover","Florida":"Tallahassee","Georgia":"Atlanta","Hawaii":"Honolulu","Idaho":"Boise","Illinois":"Springfield","Indiana":"Indianapolis","Iowa":"Des Moines","Kansas":"Topeka","Kentucky":"Frankfort","Louisiana":"Baton Rouge","Maine":"Augusta","Maryland":"Annapolis","Massachusetts":"Boston","Michigan":"Lansing","Minnesota":"Saint Paul","Mississippi":"Jackson","Missouri":"Jefferson City","Montana":"Helana","Nebraska":"Lincoln","Nevada":"Carson City","New Hampshire":"Concord","New Jersey":"Trenton","New Mexico":"Santa Fe","New York":"Albany","North Carolina":"Raleigh","North Dakota":"Bismarck","Ohio":"Columbus","Oklahoma":"Oklahoma City","Oregon":"Salem","Pennsylvania":"Harrisburg","Rhode Island":"Providence","South Carolina":"Columbia","South Dakota":"Pierre","Tennessee":"Nashville","Texas":"Austin","Utah":"Salt Lake City","Vermont":"Montpelier","Virginia":"Richmond","Washington":"Olympia","West Virginia":"Charleston","Wisconsin":"Madison","Wyoming":"Cheyenne"}
+
+//console.log(map.Alabama) //shows Montgomery
+
+
+function isCapitalCity (theState, theCapital) {
+   if (theCapital === map[theState]) {
+       return true;
+   } else {return false};
+}
+
+/*
+console.log(isCapitalCity('Texas', 'Austin'))
+console.log(isCapitalCity('Texas', 'Houston'))
+console.log(isCapitalCity('Alaska', 'Juneau'))
+console.log(isCapitalCity('Strawberry','Mango'))
+*/
--- exercises/02-strings.js
+++ exercises/02-strings.js
@@ -8,6 +8,17 @@
 // Example:
 // reverse("skoob") --> "books"
 
+function reverse (theString) {
+   var myArray = theString.split('');
+   var display = '';
+   for (i=0; i < theString.length; i++) {
+        var lastLetter = myArray.pop();
+        display = display + lastLetter
+        }
+    return display;
+}
+reverse("hello world");
+
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -17,7 +28,20 @@
 //
 // Example:
 // findLongestWord('a book full of dogs') --> 'book'
+//1:parce the sentence into an array of words; 2: if array(i)>=array(x, x goes from (0 - array.length), then return array(i), else i+1, until i=array.length
+function findLongestWord (theSentence) {
+    var myArray = theSentence.split(' ');
+    var longestWord = 0;
+    for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i].length > longestWord) {
+            longestWord = myArray[i].length;
+            var actualWord = myArray[i];
+        }
+    }
+    return actualWord;  
+}
 
+findLongestWord("The new tech-assisted approach that reuireds half the effort.");
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -29,6 +53,21 @@
 // nicer('mom get the heck in here and bring me a darn sandwich.')
 // > 'mom get the in here and bring me a sandwich.'
 
+function nicer (theSentence){
+    var sentenceArray = theSentence.split(' ');
+    var censorWords = ['heck','darn', 'dang', 'crappy'];
+
+    for (i=0; i < censorWords.length; i++) {
+        if (sentenceArray.includes(censorWords[i])) {
+           var wordIndex= sentenceArray.indexOf(censorWords[i]);
+            sentenceArray.splice(wordIndex, 1);
+        }
+    }
+    theSentence = sentenceArray.join(' ')
+    return theSentence
+}
+
+//console.log(nicer("mom get the heck in here and bring me a darn sandwich.")) passed the test
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -40,8 +79,15 @@
 // capitalizeAll('hello world') --> 'Hello World'
 // capitalizeAll('every day is like sunday') --> 'Every Day Is Like Sunday'
 
+function capitalizeAll (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+}
 
-
+  
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function called "split" that does the same thing as String.split
 // It should take two inputs: (1) a string and (2) a delimiter string
@@ -52,3 +98,24 @@
 // split('a-b-c', '-') --> ['a', 'b', 'c']
 // split('APPLExxBANANAxxCHERRY', 'xx') --> ['APPLE', 'BANANA', 'CHERRY']
 // split('xyz', 'r') --> ['xyz']
+
+//steps: look for theDelimiter in the string, slice it after the delimiter/
+//pop the dlimiter/and plus the last slice part
+
+function split (theString, theDelimiter) {
+  var newString = [];
+  var splitPosition =[];
+  for (var i = theString.indexOf(theDelimiter); i != -1; i = theString.indexOf(theDelimiter, i+1)) {
+      splitPosition.push(i); 
+  }
+  
+  newString.push(theString.substring(0,splitPosition[0]));
+  for (var j=0; j < splitPosition.length; j++) {
+      newString.push(theString.substring(splitPosition[j]+theDelimiter.length, splitPosition[j+1]));
+  }
+  return newString;
+}
+
+split("APPLExxBANANAxxCHERRY", "xx");
+split('a-b-c', '-') ;
+split('xyz', 'r');
